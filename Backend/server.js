dc7c1dfecc201200207443a975b827a860ab5fdd
Zain1906 <baigmuhammadzain@gmail.com>
2025-06-02 20:12:35 +0500

    Trying hard

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 // 1. CORS & JSON parser
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173'
+  origin: process.env.CLIENT_URL
 }));
 app.use(express.json());
 
